5c569a2dbf06eb2b4e728f3a12cc8699fc32fa81
visualstudio <visualstudio@122-PC06>
2019-04-05 19:11:44 -0400

    Section A

--- week14/Program.cs
+++ week14/Program.cs
@@ -10,60 +10,14 @@ namespace week14
 {
     class Program
     {
-        ArrayList Beowulf;
+       
 
         static void Main(string[] args)
         {
+            int Lines = File.ReadAllLines(@"U:\\Users\\732116\\beowulf.txt").Length;
+            Console.WriteLine("The number of lines in this essay are: " + Lines.ToString());
 
-            Program p = new Program();
-            p.Beowulf = new ArrayList();
-            
         }
-
-        public void Run ()
-        {
-            this.ReadTextFiles();
-        }
-
-        public void ReadTextFiles()
-        {
-            using (StreamReader file = new StreamReader("U:\\Users\\732116\\beowulf"))
-            {
-                int counter = 0;
-                String ln;
-
-                while ((ln = file.ReadLine()) ! = null){
-                    Console.WriteLine(ln);
-                    Beowulf.Add(ln);
-                }
-
-                file.Close();
-                Console.WriteLine($"File has {counter} lines.");
-
-            }
-        }
-
-        public int FindNumberOfBlankSpaces(String line)
-        {
-            int countletters = 0;
-            int countspaces = 0;
-
-            foreach (char c in line)
-            {
-                if (char.IsLetter(C))
-                {
-                    countletters++;
-                }
-                if (char.IsWhiteSpace(c))
-                {
-                    countspaces++;
-                }
-
-                return countspaces;
-
-            }
-        }
-
 
     }
 }
