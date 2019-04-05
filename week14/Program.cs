b4203e9fb692167349c41ef22148de2b38183c07
visualstudio <visualstudio@122-PC06>
2019-04-05 19:29:43 -0400

    Section A

--- week14/Program.cs
+++ week14/Program.cs
@@ -4,18 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Collections; 
+using System.Collections;
 
 namespace week14
 {
     class Program
     {
-       
+        ArrayList Beowulf;
 
         static void Main(string[] args)
         {
-            int Lines = File.ReadAllLines(@"U:\\Users\\732116\\beowulf.txt").Length;
-            Console.WriteLine("The number of lines in this essay are: " + Lines.ToString());
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+            p.ReadTextFiles();
+
+            Console.ReadLine();
+        }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader("U:\\Users\\732116\\Beowulf.txt"))
+            {
+
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+
+                counter = File.ReadLines("U:\\Users\\732116\\Beowulf.txt").Count();
+
+                Console.WriteLine("\nThe number of lines in this essay are: " + counter);
+
+
+
+            }
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+
+                if (char.IsWhiteSpace(c)) { countletters++; }
+            }
+            return countSpaces;
 
         }
 
